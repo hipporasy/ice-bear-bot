4d80299b011c0e33b75b2b4ffddcf5a4f1defe1f
Marasy Phi <hipporasy@gmail.com>
2020-12-14 01:34:52 +0700

    update send

--- commands/send.js
+++ commands/send.js
@@ -2,7 +2,9 @@ module.exports = {
     name: 'send',
     description: 'Send a message',
     async execute(message, client) {
+        console.log(message.author.id != client.user.id)
         if (message.author.id != client.user.id) return;
+        console.log('asdfadsf')
         const split = message.content.split('|');
         const serverId = split.shift()
         const messageArgs = split.split(" ")
