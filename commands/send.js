249be576044cf49be27c952940a6201fc17eecd9
Marasy Phi <hipporasy@gmail.com>
2020-12-14 02:11:40 +0700

    update send

--- commands/send.js
+++ commands/send.js
@@ -2,12 +2,11 @@ module.exports = {
     name: 'send',
     description: 'Send a message',
     async execute(message, client) {
-        console.log(message.author.id, console.log(process.env.OWNER_ID))
-        console.log(message.author.id != process.env.OWNER_ID)
         if (message.author.id != process.env.OWNER_ID) return;
-        const split = message.content.split('|');
-        const serverId = split.shift()
-        const messageArgs = split.split(" ")
+        const messageSplit = message.content.split('|');
+        const serverId = messageSplit.shift()
+        console.log(messageSplit)
+        const messageArgs = messageSplit[0].split(" ")
         const userId = messageArgs.shift()
         console.log(serverId)
         console.log(userId)
