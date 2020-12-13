0f05934710c90152ad49ba0fc8971eba09af9fda
Marasy Phi <hipporasy@gmail.com>
2020-12-14 00:37:31 +0700

    add request

---
+++ commands/loop.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "loop",
+    aliases: ["l"],
+    description: "Toggle music loop",
+    execute(message) {
+        const queue = message.client.queue.get(message.guild.id);
+        if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+
+        // toggle from false to true and reverse
+        queue.loop = !queue.loop;
+        return queue.textChannel.send(`Loop is now ${queue.loop ? "**on**" : "**off**"}`).catch(console.error);
+    }
+};
--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,11 @@
 const ytdl = require("ytdl-core");
+const YouTubeAPI = require("simple-youtube-api");
+
+const youtube = new YouTubeAPI(process.env.YouTubeAPI);
 
 module.exports = {
   name: "play",
+  aliases: ["p", "q", "queue"],
   description: "Play a song in your channel!",
   async execute(message) {
     try {
@@ -20,12 +24,63 @@ module.exports = {
           "I need the permissions to join and speak in your voice channel!"
         );
       }
-      console.log(args)
-      const songInfo = await ytdl.getInfo(args[1]);
-      const song = {
-        title: songInfo.videoDetails.title,
-        url: songInfo.videoDetails.video_url
-      };
+
+      const search = args.join(" ");
+      const videoPattern = /^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.?be)\/.+$/gi;
+      const playlistPattern = /^.*(list=)([^#\&\?]*).*/gi;
+      const mobileScRegex = /^https?:\/\/(soundcloud\.app\.goo\.gl)\/(.*)$/;
+      const url = args[0];
+      const urlValid = videoPattern.test(args[0]);
+
+      if (!videoPattern.test(args[0]) && playlistPattern.test(args[0])) {
+        return message.client.commands.get("playlist").execute(message, args);
+      }
+
+      if (mobileScRegex.test(url)) {
+        try {
+          https.get(url, function (res) {
+            if (res.statusCode == "302") {
+              return message.client.commands.get("play").execute(message, [res.headers.location]);
+            } else {
+              return message.reply("No content could be found at that url.").catch(console.error);
+            }
+          });
+        } catch (error) {
+          console.error(error);
+          return message.reply(error.message).catch(console.error);
+        }
+        return message.reply("Following url redirection...").catch(console.error);
+      }
+
+      let songInfo = null;
+      let song = null;
+
+      if (urlValid) {
+        try {
+          songInfo = await ytdl.getInfo(url);
+          song = {
+            title: songInfo.videoDetails.title,
+            url: songInfo.videoDetails.video_url,
+            duration: songInfo.videoDetails.lengthSeconds
+          };
+        } catch (error) {
+          console.error(error);
+          return message.reply(error.message).catch(console.error);
+        }
+      } else {
+        try {
+          const results = await youtube.searchVideos(search, 1);
+          songInfo = await ytdl.getInfo(results[0].url);
+          song = {
+            title: songInfo.videoDetails.title,
+            url: songInfo.videoDetails.video_url,
+            duration: songInfo.videoDetails.lengthSeconds
+          };
+        } catch (error) {
+          console.error(error);
+          return message.reply(error.message).catch(console.error);
+        }
+      }
 
       if (!serverQueue) {
         const queueContruct = {
@@ -34,6 +89,7 @@ module.exports = {
           connection: null,
           songs: [],
           volume: 5,
+          loop: false,
           playing: true
         };
         queue.set(message.guild.id, queueContruct);
@@ -75,8 +131,14 @@ module.exports = {
     const dispatcher = serverQueue.connection
       .play(ytdl(song.url))
       .on("finish", () => {
-        serverQueue.songs.shift();
-        this.play(message, serverQueue.songs[0]);
+        if (queue.loop) {
+          let lastSong = queue.songs.shift();
+          queue.songs.push(lastSong);
+          this.play(queue.songs[0], message);
+        } else {
+          queue.songs.shift();
+          this.play(queue.songs[0], message);
+        }
       })
       .on("error", error => console.error(error));
     dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
